39ccd1048dc60c271fcbb45b3c02e293e23616c5
dvillasmil08 <dvillasmil08@gmail.com>
2022-03-09 22:36:53 -0500

    added slashCommand.js and index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Followed tutorial of https://discord.js.org/#/docs/main/stable/general/welcome
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
