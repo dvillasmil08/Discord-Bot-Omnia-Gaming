afbf6a33d813c66cfe1ee774f52560521e5944dd
dvillasmil08 <dvillasmil08@gmail.com>
2022-03-09 22:39:20 -0500

    partially finish index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Followed tutorial of https://discord.js.org/#/docs/main/stable/general/welcome
 
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Interaction } = require('discord.js');
+const { intersection } = require('zod');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login('token');
