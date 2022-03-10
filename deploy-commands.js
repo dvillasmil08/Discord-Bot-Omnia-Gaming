a210ad6ea30a91576f7f0bd946ff72a4abd0bf2f
dvillasmil08 <dvillasmil08@gmail.com>
2022-03-09 22:57:03 -0500

    created deploy-commands.js with 'ping' command

---
+++ deploy-commands.js
@@ -0,0 +1,9 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+
+]
